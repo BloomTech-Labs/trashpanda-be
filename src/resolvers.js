48cb37f2544f75d55f3fb22ef5f921670c3a887e
CJ Lucido <c.jose.lucido@gmail.com>
2020-01-31 15:39:14 -0800

    added to resolvers, query for zipcode that is provided by frontend

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,6 +4,9 @@ module.exports = {
       dataSources.earthAPI.getAllMaterials(),
     material: (_, { id }, { dataSources }) =>
       dataSources.earthAPI.getMaterial({ material_id: id }),
-    families: (_, __, { dataSources }) => dataSources.earthAPI.getAllFamilies()
+    families: (_, __, { dataSources }) => dataSources.earthAPI.getAllFamilies(),
+    zipcode: (_, { zipcode }, {dataSources}) => {
+      dataSources.earthAPI.getPostalData({zipcode: zipcode})
+    }
   }
 };
