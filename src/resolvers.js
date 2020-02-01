27ad3e12bbe46951111d0abc69d69b1610ca3011
Mark Halls <mark.halls@gmail.com>
2020-01-31 17:00:43 -0700

    Add locations resolver to return all locations

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,6 +2,8 @@ module.exports = {
   Query: {
     materials: (_, __, { dataSources }) =>
       dataSources.earthAPI.getAllMaterials(),
-    families: (_, __, { dataSources }) => dataSources.earthAPI.getAllFamilies()
+    families: (_, __, { dataSources }) => dataSources.earthAPI.getAllFamilies(),
+    locations: (_, { latitude, longitude }, { dataSources }) =>
+      dataSources.earthAPI.getAllLocations({ latitude, longitude })
   }
 };
