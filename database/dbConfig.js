1104ff716e0b0d260badf3e9ea9ecca8f948f662
CJ Lucido <c.jose.lucido@gmail.com>
2020-02-01 14:40:42 -0800

    added change to dbconfig file so that knex has time to find the environment for jest (jest was outpacing it and client was returning undefined)

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,10 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 
 
-module.exports = knex(knexConfig[process.env.NODE_ENV]);
+const dbEnvironment = async () =>  {
+    return knex(knexConfig[process.env.NODE_ENV])
+}
+
+module.exports = dbEnvironment;
 
 
