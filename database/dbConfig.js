60da68a96ab29c828f145eb3316e65b49df7d52f
CJ Lucido <c.jose.lucido@gmail.com>
2020-02-03 14:07:39 -0800

    renamed env variable to DB_ENV

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,10 +1,13 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile.js");
-require("dotenv").config();
+// require("dotenv").config();
 // const dbEnvironment = async () =>  {
 //     return knex(knexConfig[process.env.NODE_ENV])
 // }
 
-module.exports = knex(knexConfig["development"]);
+const config = process && process.env && process.env.DB_ENV || "development";
+// console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+
+module.exports = knex(knexConfig[config]);
 
 
