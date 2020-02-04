f4526266ba5e9b65f6d4ebca8d9379edbe953bf3
CJ Lucido <c.jose.lucido@gmail.com>
2020-02-04 14:11:02 -0800

    Created separate mockClientQuery file, wrote tests for: the existence of queries on the schema, and type structures on the schema

---
+++ src/__tests__/mock_data/mockClientQueries.js
@@ -0,0 +1,91 @@
+const gql = require('graphql-tag');
+
+const mockClient = {
+    materials: gql`
+    query {
+            materials{
+                material_id
+                description
+                material_id
+                long_description
+                notes
+                dropoff
+                pickup
+                bin_trash
+                bin_recycle
+                bin_compost
+            }
+      }
+    `,
+    material: gql`
+    query {
+            material{
+                material_id
+                description
+                material_id
+                long_description
+                notes
+                dropoff
+                pickup
+                bin_trash
+                bin_recycle
+                bin_compost
+            }
+      }
+    `,
+    families: gql`
+    query {
+        families {
+            material_ids
+            family_id
+            description
+            family_type_id
+        }
+    }
+    `,
+    family: gql`
+    query {
+        family{
+            material_ids
+            family_id
+            description
+            family_type_id
+        }
+    }`,
+    postal_code: gql`
+    query {
+        postal_code(postal_code: "can be any string", country: "US"){
+            postal_code
+            longitude
+            latitude
+        }
+    }`,
+    locations: gql`
+    query{
+        locations(latitude: 26.8888, longitude: 23.777){
+            curbside
+            municipal
+            description
+            longitude
+            latitude
+            address
+            city
+            province
+            country
+            postal_code
+            region
+            full_address
+            national
+            location_type_id
+            event_only
+            fax
+            hours
+            phone
+            notes_public
+            url
+        }
+    }`
+}
+
+
+module.exports = mockClient 
--- src/__tests__/schema.js
+++ src/__tests__/schema.js
@@ -6,41 +6,124 @@ const {createTestClient} = require('apollo-server-testing');
 
 const mockResponses = require("./mock_data/mockSchemaResponses")
 
-const mockClient = gql`
-query {
-        materials{
-            material_id
-            description
-            material_id
-            long_description
-            notes
-            dropoff
-            pickup
-            bin_trash
-            bin_recycle
-            bin_compost
-        }
-  }
-`
+const mockClient = require('./mock_data/mockClientQueries')
+
+//the mockResolvers must match REAL queries outlined in the schema or they will not work (implied tests of Query definitions)
 
 describe('testing typeDefs', () => {
+        describe('materials query', () => {
+            it('queries materials using Material type and materials query on schema', async () => {
+                const mockResolvers = {
+                    Query:{
+                        materials: async () => {
+                            return mockResponses.mockMaterials
+                        }
+                    }
+                };
+                const mockServer = new ApolloServer({
+                    typeDefs,
+                    resolvers: mockResolvers
+                })
+                const {query} = createTestClient(mockServer)
+                const res = await query({query: mockClient.materials})
+                expect(res.data.materials).toEqual(mockResponses.mockMaterialsResponse);
+            });
 
-  it('queries mock server using typeDefs and returns variables asked for - only material_ids', async () => {
-    let mockDatasource =  mockResponses
-    const mockResolvers = {
-        Query:{
-            materials: async () => {
-                return mockDatasource.mockMaterials
-            }
-        }
-    };
-    const mockServer = new ApolloServer({
-      //TESTING typedefs
-        typeDefs,
-        resolvers: mockResolvers
-    })
-    const {query} = createTestClient(mockServer)
-    const res = await query({query: mockClient})
-     expect(res.data.materials).toEqual(mockResponses.mockMaterialsResponse);
-  });
+        });
+ 
+        describe('material query', () => {
+            it('queries material using Material type and material query on schema', async () => {
+                const mockResolvers = {
+                    Query:{
+                        material: async () => {
+                            return mockResponses.mockMaterial
+                        }
+                    }
+                };
+                const mockServer = new ApolloServer({
+                    typeDefs,
+                    resolvers: mockResolvers
+                })
+                const {query} = createTestClient(mockServer)
+                const res = await query({query: mockClient.material})
+                expect(res.data.material).toEqual(mockResponses.mockMaterialResponse);
+            });
+        });
+  
+        describe('families query', () => {
+            it('queries families using Family type and families query on schema', async () => {
+                const mockResolvers = {
+                    Query:{
+                        families: async () => {
+                            return mockResponses.mockFamilies
+                        }
+                    }
+                };
+                const mockServer = new ApolloServer({
+                    typeDefs,
+                    resolvers: mockResolvers
+                })
+                const {query} = createTestClient(mockServer)
+                const res = await query({query: mockClient.families})
+                expect(res.data.families).toEqual(mockResponses.mockFamiliesResponse);
+            });
+        });
+
+
+        describe('family query', () => {
+            it('queries family using Family type and (the existence of) family query on schema', async () => {
+                const mockResolvers = {
+                    Query:{
+                        family: async () => {
+                            return mockResponses.mockFamily
+                        }
+                    }
+                };
+                const mockServer = new ApolloServer({
+                    typeDefs,
+                    resolvers: mockResolvers
+                })
+                const {query} = createTestClient(mockServer)
+                const res = await query({query: mockClient.family})
+                expect(res.data.family).toEqual(mockResponses.mockFamilyResponse);
+            });
+        });
+
+        describe('postal_code query', () => {
+            it('queries postal_code using Postal_code type and (the existence of) postal_code query on schema', async () => {
+                const mockResolvers = {
+                    Query:{
+                        postal_code: async () => {
+                            return mockResponses.mockPostalCode
+                        }
+                    }
+                };
+                const mockServer = new ApolloServer({
+                    typeDefs,
+                    resolvers: mockResolvers
+                })
+                const {query} = createTestClient(mockServer)
+                const res = await query({query: mockClient.postal_code})
+                expect(res.data.postal_code).toEqual(mockResponses.mockPostalCodeResponse);
+            });
+        });
+
+        describe('locations query', () => {
+            it('queries locations using Location type and (the existence of) locations query on schema', async () => {
+                const mockResolvers = {
+                    Query:{
+                        locations: async () => {
+                            return mockResponses.mockLocations
+                        }
+                    }
+                };
+                const mockServer = new ApolloServer({
+                    typeDefs,
+                    resolvers: mockResolvers
+                })
+                const {query} = createTestClient(mockServer)
+                const res = await query({query: mockClient.locations})
+                expect(res.data.locations).toEqual(mockResponses.mockLocationsResponse);
+            });
+        });
 });
