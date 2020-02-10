75939997bae702bc1e8f18b301615413e9de9344
Mark Artishuk <markyshuk@gmail.com>
2020-02-10 11:04:42 -0800

    Adding getZip query test case

--- src/datasources/__tests__/mapquest.js
+++ src/datasources/__tests__/mapquest.js
@@ -1,9 +1,12 @@
 const mockData = require("./mock_data/mockMapquestResponses");
 const MapQuestAPI = require("../mapquest");
 
-const mocks = {};
+const mocks = {
+  get: jest.fn()
+};
 
 const datasource = new MapQuestAPI();
+datasource.get = mocks.get;
 
 //Reducers
 describe("[MapQuestAPI Reducers]", () => {
@@ -12,6 +15,18 @@ describe("[MapQuestAPI Reducers]", () => {
       expect(datasource.postalDataReducer(mockData.mockLocation)).toEqual(
         mockData.mockPostalDataReduced
       );
+    });
+  });
+});
+
+//Queries
+describe("[MapQuestAPI Queries]", () => {
+  describe("getZip", () => {
+    it("gets zip code from latitude/longitude", async () => {
+      mocks.get.mockReturnValueOnce(mockData.mockResponseData);
+
+      const res = await datasource.getZip();
+      expect(res).toMatchObject(mockData.mockPostalDataReduced);
     });
   });
 });
