cb7a8ba2eaddab06f7286295d5bceaab9c67d985
Mark Artishuk <markyshuk@gmail.com>
2020-02-13 14:21:20 -0800

    Adding migration to add image_url column

---
+++ database/migrations/20200213140924_category_images_table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.table("materials", table => {
+    table.string("image_url");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table("materials", table => {
+    table.dropColumn("image_url");
+  });
+};
