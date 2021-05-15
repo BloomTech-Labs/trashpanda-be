9c34dbb340cffd48c0adffdf6f506f46e05cb6d0
Mark Artishuk <markyshuk@gmail.com>
2021-05-14 18:54:30 -0700

    back to reject unauthorized

--- knexfile.js
+++ knexfile.js
@@ -45,7 +45,9 @@ module.exports = {
     client: "pg",
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     migrations: {
       directory: "./database/migrations",
